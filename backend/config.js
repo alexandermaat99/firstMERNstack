a09fb63f65179ed6519b823db542342a9af7e748
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-06-10 13:23:29 -0600

    fixed

--- backend/config.js
+++ backend/config.js
@@ -1,33 +1,3 @@
 export const PORT = 5555;
 export const mongoDBURL =
-  "mongodb+srv://root:<4AhnRN6DuAB2thw>@mern.ba0btee.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN";
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-const uri =
-  "mongodb+srv://root:<4AhnRN6DuAB2thw>@mern.ba0btee.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+  "://localhost:27017/express-mongomongodb+srv://root:<4AhnRN6DuAB2thw>@mern.ba0btee.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN";
