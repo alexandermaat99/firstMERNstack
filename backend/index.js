e403c2c93c46b59c0c01e38b507dfa10aacdc8f5
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-06-10 12:48:09 -0600

    init 2

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hello from the server");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
