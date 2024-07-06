977311b508310b8f4feeb935a5c2a4094fb52b52
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-07-06 13:41:10 -0600

    fixing stuff

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,24 @@ app.get("/", (req, res) => {
   return res.status(234).send("Hello from the server");
 });
 
+// route for save a new book 
+app.post('/books', async (req, res) => {
+  try {
+    if(
+      !request.body.title || 
+      !request.body.author || 
+      !request.body.publisherYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publisherYear',
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({message: error.message{});
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
